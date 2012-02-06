271ca19cfe74a0c7b10e6c84b4be27faf74c2ef8
Roland Schulz <roland@utk.edu>
2012-02-05 19:07:44 -0500

    Clean after checkout
    Change-Id: I53d7e4de12bb15ddf71f713a3fc949a88c25da7c

--- admin/GerritBuild.bat
+++ admin/GerritBuild.bat
@@ -1,3 +1,3 @@
 mkdir gromacs
 cd gromacs
-git init && git fetch ssh://jenkins@gerrit.gromacs.org:29418/gromacs refs/heads/release-4-5-patches && git checkout -q -f FETCH_HEAD && python -u ../GerritBuild.py
+git init && git fetch ssh://jenkins@gerrit.gromacs.org:29418/gromacs refs/heads/release-4-5-patches && git checkout -q -f FETCH_HEAD && git clean -f && python -u ../GerritBuild.py
